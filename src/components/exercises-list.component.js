7bef10570d0a598854b4e6a94b29194fb6163375
sai vamsi yelamanchili <44221627+wonderfulvamsi@users.noreply.github.com>
2022-02-17 20:43:40 +0530

    Update exercises-list.component.js

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -51,7 +51,7 @@ export default class ExercisesList extends Component {
   render() {
     return (
       <div>
-        <h3>Logged Exercises</h3>
+        <h3>Hightlights So Far..</h3>
         <table className="table">
           <thead className="thead-light">
             <tr>
