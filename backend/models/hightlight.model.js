12c98ee74e93b7c0537894a72b7ca01495b149d1
sai vamsi yelamanchili <44221627+wonderfulvamsi@users.noreply.github.com>
2022-02-18 22:41:28 +0530

    Update and rename exercise.model.js to hightlight.model.js

--- backend/models/hightlight.model.js
+++ backend/models/hightlight.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
+const hightlightSchema = new Schema({
   username: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
@@ -11,6 +11,6 @@ const exerciseSchema = new Schema({
   timestamps: true,
 });
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+const Hightlight = mongoose.model('Hightlight', hightlightSchema);
 
-module.exports = Exercise;
+module.exports = Hightlight;
