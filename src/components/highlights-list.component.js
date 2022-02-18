32785581121bb29c8239c5563ee046630b4a2f72
sai vamsi yelamanchili <44221627+wonderfulvamsi@users.noreply.github.com>
2022-02-18 22:58:05 +0530

    Update

---
+++ src/components/highlights-list.component.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Highlight = props => (
+  <tr>
+    <td>{props.highlight.username}</td>
+    <td>{props.highlight.description}</td>
+    <td>{props.highlight.duration}</td>
+    <td>{props.highlight.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.highlight._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.highlight._id) }}>delete</a>
+    </td>
+  </tr>
+)
+
+export default class HighlightsList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteHighlight = this.deleteHighlight.bind(this)
+
+    this.state = { highlights: [] };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/highlights/')
+      .then(response => {
+        this.setState({ highlights: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteHighlight(id) {
+    axios.delete('http://localhost:5000/highlights/' + id)
+      .then(response => { console.log(response.data) });
+
+    this.setState({
+      highlights: this.state.highlights.filter(el => el._id !== id)
+    })
+  }
+
+  highlightList() {
+    return this.state.highlights.map(currenthighlight => {
+      return <Highlight highlight={currenthighlight} deleteHighlight={this.deleteHighlight} key={currenthighlight._id} />;
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Hightlights So Far..</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Book name</th>
+              <th>Insight</th>
+              <th>Chapter</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.highlightList()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
