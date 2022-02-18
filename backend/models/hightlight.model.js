65b650ea280709b20175fe71c653128b1a8d363e
sai vamsi yelamanchili <44221627+wonderfulvamsi@users.noreply.github.com>
2022-02-18 22:51:44 +0530

    Update hightlight.model.js

--- backend/models/hightlight.model.js
+++ backend/models/hightlight.model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const hightlightSchema = new Schema({
-  username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
+  bookname: { type: String, required: true },
+  insight: { type: String, required: true },
+  chapter: { type: Number, required: true },
   date: { type: Date, required: true },
 }, {
   timestamps: true,
